828bde3929006f7f328bcae50aa208319ee20d5b
Phuoc Nhat <doanphuocnhat205@gmail.com>
2022-10-16 23:29:35 +0700

    Second commit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,22 @@
-import React from "react";
+import IconButton from "./IconButton";
+import {
+    HomeIcon,
+    HeartIcon,
+    LibraryIcon,
+} from "@styled-icons/heroicons-outline";
 
 const Sidebar = () => {
-    return <div>Side bar</div>;
+    return (
+        <div className="text-gray-500 px-5 pt-5 pb-36 text-xs lg:text-sm border-r border-gray-900 h-screen overflow-y-scroll sm:max-w-[12rem] lg:max-w-[15rem] hidden md:block">
+            <div className="space-y-4">
+                <IconButton icon={HomeIcon} label="Home" />
+                <IconButton icon={HomeIcon} label="Home" />
+                <IconButton icon={HomeIcon} label="Home" />
+                <IconButton icon={HomeIcon} label="Home" />
+                <IconButton icon={HomeIcon} label="Home" />
+                <IconButton icon={HomeIcon} label="Home" />
+            </div>
+        </div>
+    );
 };
 export default Sidebar;
