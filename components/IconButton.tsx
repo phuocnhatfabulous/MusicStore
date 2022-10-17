ba2c5c797404db17074b15acea9d194ae783d67d
Phuoc Nhat <doanphuocnhat205@gmail.com>
2022-10-17 11:53:49 +0700

    Tailwind Convention

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -8,7 +8,7 @@ interface Props {
 const IconButton = ({ icon: Icon, label }: Props) => {
     return (
         <button className="flex items-center space-x-2 hover:text-white">
-            <Icon className="h-5 w-5" />
+            <Icon className="icon" />
             <span>{label}</span>
         </button>
     );
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,20 +3,52 @@ import {
     HomeIcon,
     HeartIcon,
     LibraryIcon,
-} from "@styled-icons/heroicons-outline";
+    PlusCircleIcon,
+    RssIcon,
+    SearchIcon,
+} from "@heroicons/react/outline";
 
-const Sidebar = () => {
+const Divider = () => <hr className="border-t-[0.1px] border-gray-900" />;
+function Sidebar(): JSX.Element {
     return (
-        <div className="text-gray-500 px-5 pt-5 pb-36 text-xs lg:text-sm border-r border-gray-900 h-screen overflow-y-scroll sm:max-w-[12rem] lg:max-w-[15rem] hidden md:block">
+        <div className="text-gray-500 px-5 pt-5 pb-36 text-xs lg:text-sm border-r border-gray-900 h-screen overflow-y-scroll scrollbar-hidden sm:max-w-[12rem] lg:max-w-[15rem] hidden md:block">
             <div className="space-y-4">
                 <IconButton icon={HomeIcon} label="Home" />
-                <IconButton icon={HomeIcon} label="Home" />
-                <IconButton icon={HomeIcon} label="Home" />
-                <IconButton icon={HomeIcon} label="Home" />
-                <IconButton icon={HomeIcon} label="Home" />
-                <IconButton icon={HomeIcon} label="Home" />
+                <IconButton icon={SearchIcon} label="Search" />
+                <IconButton icon={LibraryIcon} label="Your Library" />
+                <Divider />
+                <IconButton icon={PlusCircleIcon} label="Create Playlist" />
+                <IconButton icon={HeartIcon} label="Liked Songs" />
+                <IconButton icon={RssIcon} label="Your episodes" />
+                <Divider />
+
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
             </div>
         </div>
     );
-};
+}
 export default Sidebar;
