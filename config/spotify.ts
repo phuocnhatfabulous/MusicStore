2b982070a71dba064bbe365c3b0b307beb6d1e7b
Phuoc Nhat <doanphuocnhat205@gmail.com>
2022-10-17 11:51:36 +0700

    Call api Spotify

---
+++ config/spotify.ts
@@ -0,0 +1,22 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
+const scopes = [
+    "user-read-email",
+    "user-read-private",
+    "user-library-read",
+    "user-top-read",
+    "user-read-playback-state",
+    "user-modify-playback-state",
+    "user-read-currently-played",
+    "user-read-recently-played",
+    "user-follow-read",
+    "playlist-read-private",
+    "playlist-read-collaborative",
+    "stream",
+].join(",");
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+});
+export { spotifyApi, scopes };
