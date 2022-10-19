9340d2347e0fc00b090833cb377e2fda64dfdbc1
Phuoc Nhat <doanphuocnhat205@gmail.com>
2022-10-19 23:24:52 +0700

    Push data

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,25 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Sidebar from "../components/Sidebar";
 import Center from "../components/Center";
+import PlaylistContextProvider from "../contexts/PlaylistContext";
 
 const Home: NextPage = () => {
     return (
         <div className="bg-black h-screen overflow-hidden">
-            <Head>
-                <title>Spotify</title>
-                <meta name="description" content="Spotify by P.N fabulous" />
-                <link rel="icon" href="" />
-            </Head>
-            <main className="flex">
-                <Sidebar />
-                <Center />
-            </main>
+            <PlaylistContextProvider>
+                <Head>
+                    <title>Spotify</title>
+                    <meta
+                        name="description"
+                        content="Spotify by P.N fabulous"
+                    />
+                    <link rel="icon" href="" />
+                </Head>
+                <main className="flex">
+                    <Sidebar />
+                    <Center />
+                </main>
+            </PlaylistContextProvider>
         </div>
     );
 };
