febc02a7bf8ebae80af4ad852d24573a183a4eb3
Phuoc Nhat <doanphuocnhat205@gmail.com>
2022-10-19 23:13:28 +0700

    Set up data

--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,28 @@
-import { User } from "next-auth";
+import { Session, User } from "next-auth";
 import { JWT } from "next-auth/jwt";
 
+export enum TokenError {
+    RefreshAccessTokenError = "RefreshAccessTokenError",
+}
 export interface ExtendedToken extends JWT {
     accessToken: string;
     refreshToken: string;
     accessTokenExpiresAt: number;
     user: User;
+    error?: TokenError;
+}
+export interface ExtendedSession extends Session {
+    accessToken: ExtendedToken["accessToken"];
+    error: ExtendedToken["error"];
+}
+export interface PlaylistContextState {
+    playlists: SpotifyApi.PlaylistObjectSimplified[];
+    selectedPlaylistId: string | null;
+    selectedPlaylist: SpotifyApi.SinglePlaylistResponse | null;
+}
+export interface IPlaylistContext {
+    playlistContextState: PlaylistContextState;
+    updatePlaylistContextState: (
+        updateObj: Partial<PlaylistContextState>
+    ) => void;
 }
