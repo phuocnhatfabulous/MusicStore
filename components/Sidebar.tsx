250ed83330a32db72184e54a7ab41906d830446d
Phuoc Nhat <doanphuocnhat205@gmail.com>
2022-10-19 23:28:01 +0700

    Display sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,10 +8,24 @@ import {
     SearchIcon,
 } from "@heroicons/react/outline";
 import { useSession } from "next-auth/react";
+import { usePlaylistContext } from "../contexts/PlaylistContext";
+import useSpotify from "../hooks/useSpotify";
 
 const Divider = () => <hr className="border-t-[0.1px] border-gray-900" />;
 const Sidebar = () => {
     const { data: session } = useSession();
+    const spotifyApi = useSpotify();
+    const {
+        playlistContextState: { playlists },
+        updatePlaylistContextState,
+    } = usePlaylistContext();
+    const setSelectedPlaylist = async (playlistId: string) => {
+        const playlistResponse = await spotifyApi.getPlaylist(playlistId);
+        updatePlaylistContextState({
+            selectedPlaylistId: playlistId,
+            selectedPlaylist: playlistResponse.body,
+        });
+    };
     return (
         <div className="text-gray-500 px-5 pt-5 pb-36 text-xs lg:text-sm border-r border-gray-900 h-screen overflow-y-scroll scrollbar-hidden sm:max-w-[12rem] lg:max-w-[15rem] hidden md:block">
             {session?.user && <button>{session.user.name} - Log Out</button>}
@@ -24,32 +38,18 @@ const Sidebar = () => {
                 <IconButton icon={HeartIcon} label="Liked Songs" />
                 <IconButton icon={RssIcon} label="Your episodes" />
                 <Divider />
-
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
-                <p className="curson-pointer hover:text-white">PLAYPLIST</p>
+                {/* Playlists */}
+                {playlists.map(({ id, name }) => (
+                    <p
+                        key={id}
+                        className="cursor-pointer hover:text-while"
+                        onClick={() => {
+                            setSelectedPlaylist(id);
+                        }}
+                    >
+                        {name}
+                    </p>
+                ))}
             </div>
         </div>
     );
