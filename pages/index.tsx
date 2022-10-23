2f451fc5d294a237f135466fa2b7734fc6de05d5
Phuoc Nhat <doanphuocnhat205@gmail.com>
2022-10-23 16:14:57 +0700

    Add btn functions

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,30 @@ import Head from "next/head";
 import Sidebar from "../components/Sidebar";
 import Center from "../components/Center";
 import PlaylistContextProvider from "../contexts/PlaylistContext";
+import Player from "../components/Player";
+import SongContextProvider from "../contexts/SongContext";
 
 const Home: NextPage = () => {
     return (
         <div className="bg-black h-screen overflow-hidden">
             <PlaylistContextProvider>
-                <Head>
-                    <title>Spotify</title>
-                    <meta
-                        name="description"
-                        content="Spotify by P.N fabulous"
-                    />
-                    <link rel="icon" href="" />
-                </Head>
-                <main className="flex">
-                    <Sidebar />
-                    <Center />
-                </main>
+                <SongContextProvider>
+                    <Head>
+                        <title>Spotify</title>
+                        <meta
+                            name="description"
+                            content="Spotify by P.N fabulous"
+                        />
+                        <link rel="icon" href="" />
+                    </Head>
+                    <main className="flex">
+                        <Sidebar />
+                        <Center />
+                    </main>
+                    <div className="sticky bottom-0 text-white">
+                        <Player />
+                    </div>
+                </SongContextProvider>
             </PlaylistContextProvider>
         </div>
     );
