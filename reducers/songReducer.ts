aec22c7878f0a4be499ce5c4d1f4b74bd875f846
Phuoc Nhat <doanphuocnhat205@gmail.com>
2022-10-23 16:11:36 +0700

    reducers

---
+++ reducers/songReducer.ts
@@ -0,0 +1,39 @@
+import {
+    SongContextState,
+    SongReducerAction,
+    SongReducerActionType,
+} from "../types";
+
+export const songReducer = (
+    state: SongContextState,
+    { type, payload }: SongReducerAction
+): SongContextState => {
+    switch (type) {
+        case SongReducerActionType.SetDevice:
+            return {
+                ...state,
+                deviceId: payload.deviceId,
+                volume: payload.volume,
+            };
+        case SongReducerActionType.ToggleIsPlaying:
+            return {
+                ...state,
+                isPlaying: payload,
+            };
+        case SongReducerActionType.SetCurrentPlayingSong:
+            const { selectedSongId, selectedSong, isPlaying } = payload;
+            return {
+                ...state,
+                selectedSongId,
+                selectedSong,
+                isPlaying,
+            };
+        case SongReducerActionType.SetVolume:
+            return {
+                ...state,
+                volume: payload,
+            };
+        default:
+            return state;
+    }
+};
